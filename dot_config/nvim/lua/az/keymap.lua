31b0501483adaa503cf1c6e783a4745247aefc01
Anton Ždanov <anton@azdanov.dev>
2024-02-04 20:13:17 +0200

    remove keymaps

--- dot_config/nvim/lua/az/keymap.lua
+++ dot_config/nvim/lua/az/keymap.lua
@@ -9,9 +9,3 @@ vim.keymap.set({ "n", "x" }, "<leader>rd", [["_d]], { desc = "Delete without yan
 
 vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected block down" })
 vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected block up" })
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>", { desc = "Next item in the quickfix list" })
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>", { desc = "Previous item in the quickfix list" })
-
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>", { desc = "Next item in the location list" })
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>", { desc = "Previous item in the location list" })
