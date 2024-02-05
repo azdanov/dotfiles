fc5b1c35e261f4e1e00c8160283fd842b21c5f4f
Anton Ždanov <anton@azdanov.dev>
2024-02-05 20:21:21 +0200

    update conform config

--- dot_config/nvim/lua/az/plugins/conform.lua
+++ dot_config/nvim/lua/az/plugins/conform.lua
@@ -1,26 +1,43 @@
-local prettier = { "prettierd", "prettier" }
 return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format",
+    },
+  },
   opts = {
     formatters_by_ft = {
-      javascript = prettier,
-      typescript = prettier,
-      javascriptreact = prettier,
-      typescriptreact = prettier,
-      css = prettier,
-      html = prettier,
-      json = prettier,
-      jsonc = prettier,
-      yaml = prettier,
-      markdown = prettier,
-      graphql = prettier,
+      javascript = { "prettierd", "prettier" },
+      typescript = { "prettierd", "prettier" },
+      javascriptreact = { "prettierd", "prettier" },
+      typescriptreact = { "prettierd", "prettier" },
+      css = { "prettierd", "prettier" },
+      html = { "prettierd", "prettier" },
+      json = { "prettierd", "prettier" },
+      jsonc = { "prettierd", "prettier" },
+      yaml = { "prettierd", "prettier" },
+      markdown = { "prettierd", "prettier" },
+      graphql = { "prettierd", "prettier" },
       go = { "gofmt" },
       rust = { "rustfmt" },
       lua = { "stylua" },
     },
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2" },
+      },
+    },
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
     log_level = vim.log.levels.DEBUG,
   },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
