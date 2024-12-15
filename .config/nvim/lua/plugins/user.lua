97020fb035460efcf552221f881c38d90e83d4e8
Anton Ždanov <anton@azdanov.dev>
2024-12-15 16:01:29 +0200

    chore: update plugins

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -4,12 +4,22 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    opts = function(_, opts)
-      opts.background = {
+    opts = {
+      background = {
         light = "latte",
         dark = "macchiato",
-      }
-      if vim.g.neovide then opts.term_colors = true end
-    end,
+      },
+      integrations = { blink_cmp = true },
+      term_colors = function() return vim.g.neovide end,
+    },
+  },
+  {
+    "Saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      },
+    },
   },
 }
