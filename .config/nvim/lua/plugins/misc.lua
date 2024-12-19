51e1a173af025a9654e7e4b11e2de994a4bd1600
Anton Ždanov <anton@azdanov.dev>
2024-12-19 07:08:08 +0200

    fix: nvim plugins

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -18,9 +18,17 @@ return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   {
+    "zbirenbaum/copilot.lua",
+    optional = true,
+    opts = {
+      filetypes = { ["*"] = true },
+    },
+  },
+  {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       window = {
+        width = 30,
         mappings = {
           ["z"] = "none",
         },
