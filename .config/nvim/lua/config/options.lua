9898f11513d8ce44967d0499d452114cb8a268c9
Anton Ždanov <anton@azdanov.dev>
2024-12-21 15:18:06 +0200

    feat: update nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+if vim.g.neovide then
+  vim.o.guifont = "MonoLisa Medium,Symbols Nerd Font:h34"
+  vim.g.neovide_scale_factor = 0.4
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.snacks_animate = false
+end
--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -3,12 +3,23 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
       window = {
         width = 30,
         mappings = {
           ["z"] = "none",
         },
       },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
     },
   },
 }
