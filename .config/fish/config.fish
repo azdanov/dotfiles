8b495a38dee29d9dd76118adeaba1c5b98a117b6
Anton Ždanov <anton@azdanov.dev>
2024-12-30 14:45:24 +0200

    feat: add alias for lazygit yadm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,8 @@ abbr -a -- vim nvim
 abbr -a -- nvide neovide
 abbr -a -- gg lazygit
 
+alias yl='lazygit --work-tree ~ --git-dir ~/.local/share/yadm/repo.git'
+
 set -gx EDITOR nvim
 set -gx MANPAGER 'nvim +Man!'
 set -gx LG_CONFIG_FILE "$HOME/.config/lazygit/tokyonight_day.yml"
