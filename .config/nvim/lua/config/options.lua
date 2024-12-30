09a16167ef21d3f6f956a3830e7faf286008717a
Anton Ždanov <anton@azdanov.dev>
2024-12-30 14:45:44 +0200

    chore: update nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,8 +3,6 @@
 -- Add any additional options here
 
 if vim.g.neovide then
-  vim.o.guifont = "MonoLisa Medium,Symbols Nerd Font:h34"
-  vim.g.neovide_scale_factor = 0.4
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.snacks_animate = false
 end
