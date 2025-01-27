e42db99963669c1946a8ba2553b0e40b54d05b84
Anton Ždanov <anton@azdanov.dev>
2025-01-27 18:57:30 +0200

    update fzf env opts

--- .config/fish/functions/theme_dark.fish
+++ .config/fish/functions/theme_dark.fish
@@ -9,7 +9,7 @@ function theme_dark
 
     set -e -g LS_COLORS FZF_DEFAULT_OPTS LG_CONFIG_FILE BAT_THEME
     set -Ux LS_COLORS (vivid generate astrodark)
-    set -Ux FZF_DEFAULT_OPTS "--highlight-line --info=inline-right --ansi --layout=reverse --border=none --color=bg+:#1E222A --color=bg:#1A1D23 --color=border:#3A3E47 --color=fg:#ADB0BB --color=gutter:#1A1D23 --color=header:#50A4E9 --color=hl+:#5EB7FF --color=hl:#5EB7FF --color=info:#3A3E47 --color=marker:#5EB7FF --color=pointer:#5EB7FF --color=prompt:#5EB7FF --color=query:#ADB0BB:regular --color=scrollbar:#3A3E47 --color=separator:#3A3E47 --color=spinner:#5EB7FF"
+    set -Ux FZF_DEFAULT_OPTS "--highlight-line --info=inline-right --ansi --layout=reverse --border=none --color=bg+:#1E222A --color=bg:#1A1D23 --color=border:#3A3E47 --color=fg+:#ADB0BB --color=fg:#ADB0BB --color=gutter:#1A1D23 --color=header:#50A4E9 --color=hl+:#5EB7FF --color=hl:#5EB7FF --color=info:#3A3E47 --color=marker:#5EB7FF --color=pointer:#5EB7FF --color=prompt:#5EB7FF --color=query:#ADB0BB:regular --color=scrollbar:#3A3E47 --color=separator:#3A3E47 --color=spinner:#5EB7FF"
     set -Ux LG_CONFIG_FILE "$HOME/.config/lazygit/astrodark.yml"
     set -Ux BAT_THEME astrodark
 
