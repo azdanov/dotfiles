3f06f43eea5cfa3446d3135056ec4526ea4a44c8
Anton Ždanov <anton@azdanov.dev>
2025-02-08 18:23:36 +0200

    upgrade lazyvim version

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,5 +1,4 @@
 return {
-  "tpope/vim-rsi",
   {
     "neovim/nvim-lspconfig",
     opts = {
@@ -11,41 +10,6 @@ return {
     opts = {
       ensure_installed = {
         "gotmpl",
-      },
-    },
-  },
-  {
-    "folke/noice.nvim",
-    keys = {
-      -- disable the default keymap that conflict with backward and forward navigation
-      {
-        "<c-f>",
-        mode = { "i", "n", "s" },
-        false,
-      },
-      {
-        "<c-b>",
-        mode = { "i", "n", "s" },
-        false,
-      },
-    },
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_by_name = {
-            ".git",
-          },
-        },
-      },
-      window = {
-        width = 30,
-        mappings = {
-          ["z"] = "none",
-        },
       },
     },
   },
