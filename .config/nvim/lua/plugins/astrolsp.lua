1ee102f7c4bde670c19c5f4707098aeda78362dc
Anton Ždanov <anton@azdanov.dev>
2025-02-28 19:30:08 +0200

    add astrolsp with additional vtsls settings

---
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -0,0 +1,22 @@
+return {
+  "AstroNvim/astrolsp",
+  ---@type AstroLSPOpts
+  opts = {
+    ---@diagnostic disable: missing-fields
+    config = {
+      vtsls = {
+        settings = {
+          vtsls = {
+            autoUseWorkspaceTsdk = true,
+            experimental = {
+              maxInlayHintLength = 30,
+              completion = {
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
