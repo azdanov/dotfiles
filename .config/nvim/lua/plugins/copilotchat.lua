33324abdffca58db6ea7b5a70f6363e87d4f4fbf
Anton Ždanov <anton@azdanov.dev>
2025-02-28 18:38:08 +0200

    unite copilot configs

--- .config/nvim/lua/plugins/copilotchat.lua
+++
@@ -1,9 +0,0 @@
----@type LazySpec
-return {
-  "CopilotC-Nvim/CopilotChat.nvim",
-  branch = "main",
-  version = false,
-  dependencies = {
-    { "zbirenbaum/copilot.lua", optional = true, enabled = false },
-  },
-}
