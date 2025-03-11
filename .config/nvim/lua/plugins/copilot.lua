148dcb79cb22be06eff593240962fa991da12e47
Anton Ždanov <anton@azdanov.dev>
2025-03-11 22:00:53 +0200

    add copilot autocmd

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -14,13 +14,23 @@ return {
     opts = {
       options = {
         g = {
+          copilot_workspace_folders = { vim.fn.getcwd() },
           copilot_integration_id = "vscode-chat",
           copilot_settings = { selectedCompletionModel = "gpt-4o-copilot" },
-          copilot_workspace_folders = { vim.fn.getcwd() },
           copilot_filetypes = {
             markdown = true,
             ["grug-far"] = false,
             ["grug-far-history"] = false,
+          },
+        },
+      },
+      autocmds = {
+        copilot_workspaces = {
+          {
+            event = "DirChanged",
+            pattern = "*",
+            desc = "Keep Copilot workspace folders updated to current directory",
+            callback = function() vim.g.copilot_workspace_folders = { vim.fn.getcwd() } end,
           },
         },
       },
