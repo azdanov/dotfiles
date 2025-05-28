fce0638a7befdd5ca55f8327641c1ea5d4d59c2f
Anton Ždanov <anton@azdanov.dev>
2025-05-28 20:36:10 +0300

    add neo-tree dir grouping

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -3,6 +3,8 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
+      group_empty_dirs = true,
+      scan_mode = "deep",
       filtered_items = {
         hide_dotfiles = false,
         hide_by_name = {
