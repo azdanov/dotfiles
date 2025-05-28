ad6af16d57a255b7e1f1ad880051a96861369b62
Anton Ždanov <anton@azdanov.dev>
2025-05-28 20:35:34 +0300

    cleanup

--- .config/fish/functions/theme_dark.fish
+++ .config/fish/functions/theme_dark.fish
@@ -3,7 +3,6 @@ function theme_dark
 
     sed -i s/adwaita/tokyo-night/g ~/.config/btop/btop.conf
     sed -i s/astrolight/astrodark/g ~/.config/nvim/lua/plugins/astroui.lua
-    sed -i s/astrolight/astrodark/g ~/.config/fish/config.fish
 
     set -Ux LS_COLORS (vivid generate astrodark)
     set -Ux FZF_DEFAULT_OPTS "--highlight-line --info=inline-right --ansi --layout=reverse --border=none --color=bg+:#1E222A --color=bg:#1A1D23 --color=border:#3A3E47 --color=fg+:#ADB0BB --color=fg:#ADB0BB --color=gutter:#1A1D23 --color=header:#50A4E9 --color=hl+:#5EB7FF --color=hl:#5EB7FF --color=info:#3A3E47 --color=marker:#5EB7FF --color=pointer:#5EB7FF --color=prompt:#5EB7FF --color=query:#ADB0BB:regular --color=scrollbar:#3A3E47 --color=separator:#3A3E47 --color=spinner:#5EB7FF"
