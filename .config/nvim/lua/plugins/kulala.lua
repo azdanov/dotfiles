bd21b0fe651dae0e798b00566716c6577d7505ef
Anton Ždanov <anton@azdanov.dev>
2025-07-19 15:58:58 +0300

    add kulala

---
+++ .config/nvim/lua/plugins/kulala.lua
@@ -0,0 +1,32 @@
+return {
+  "mistweaverco/kulala.nvim",
+  ft = { "http", "rest" },
+  keys = function(_, keys)
+    local plugin = require("lazy.core.config").spec.plugins["kulala.nvim"]
+    local opts = require("lazy.core.plugin").values(plugin, "opts", false) -- resolve mini.clue options
+    if opts.global_keymaps_prefix then table.insert(keys, { opts.global_keymaps_prefix, desc = "Load KulalaNvim" }) end
+    return keys
+  end,
+  specs = {
+    {
+      "AstroNvim/astroui",
+      ---@type AstroUIOpts
+      opts = { icons = { KulalaNvim = "󱜿" } },
+    },
+  },
+  opts = {
+    global_keymaps = true,
+    global_keymaps_prefix = "<leader>r",
+    lsp = { on_attach = function(...) return require("astrolsp").on_attach(...) end },
+  },
+  config = function(_, opts)
+    require("kulala").setup(opts)
+    if opts.global_keymaps_prefix then
+      require("astrocore").set_mappings {
+        n = {
+          [opts.global_keymaps_prefix] = { desc = require("astroui").get_icon("KulalaNvim", 1, true) .. "KulalaNvim" },
+        },
+      }
+    end
+  end,
+}
