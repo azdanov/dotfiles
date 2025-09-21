e655bacb80ebbd48375fd99db625d01b742881b7
Anton Ždanov <anton@azdanov.dev>
2025-09-21 19:00:36 +0300

    update nvim-jdtls

--- .config/nvim/lua/plugins/nvim-jdtls.lua
+++ .config/nvim/lua/plugins/nvim-jdtls.lua
@@ -5,27 +5,20 @@ return {
   ft = { "java" },
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
-    {
-      "AstroNvim/astrolsp",
-      optional = true,
-      ---@type AstroLSPOpts
-      opts = {
-        handlers = { jdtls = false },
-      },
-    },
   },
   opts = function(_, opts)
-    local utils = require "astrocore"
-
     local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
     local workspace_dir = vim.fn.stdpath "data" .. "/site/java/workspace-root/" .. project_name
     vim.fn.mkdir(workspace_dir, "p")
 
-    if not (vim.fn.has "mac" == 1 or vim.fn.has "unix" == 1 or vim.fn.has "win32" == 1) then
-      utils.notify("jdtls: Could not detect valid OS", vim.log.levels.ERROR)
-    end
+    local extendedClientCapabilities = require("astrocore").extend_tbl(require "jdtls.capabilities", {
+      onCompletionItemSelectedCommand = "editor.action.triggerParameterHints",
+      resolveAdditionalTextEditsSupport = true,
+      workspace = { configuration = true },
+      textDocument = { completion = { completionItem = { snippetSupport = true } } },
+    })
 
-    return utils.extend_tbl({
+    return require("astrocore").extend_tbl(opts, {
       cmd = {
         "java",
         "-Declipse.application=org.eclipse.jdt.ls.core.id1",
@@ -33,13 +26,15 @@ return {
         "-Declipse.product=org.eclipse.jdt.ls.core.product",
         "-Dlog.protocol=true",
         "-Dlog.level=ALL",
-        "-javaagent:" .. vim.fn.expand "$MASON/share/jdtls/lombok.jar",
-        "-Xms1g",
+        "-XX:+UseTransparentHugePages",
+        "-XX:+AlwaysPreTouch",
+        "-Xmx2g",
         "--add-modules=ALL-SYSTEM",
         "--add-opens",
         "java.base/java.util=ALL-UNNAMED",
         "--add-opens",
         "java.base/java.lang=ALL-UNNAMED",
+        "-javaagent:" .. vim.fn.expand "$MASON/share/jdtls/lombok.jar",
         "-jar",
         vim.fn.expand "$MASON/share/jdtls/plugins/org.eclipse.equinox.launcher.jar",
         "-configuration",
@@ -48,20 +43,16 @@ return {
         workspace_dir,
       },
       root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
-      flags = {
-        allow_incremental_sync = true,
-        server_side_fuzzy_completion = true,
-      },
+      flags = { allow_incremental_sync = true, server_side_fuzzy_completion = true },
+      capabilities = require("astrolsp").config.config["*"].capabilities,
       settings = {
         java = {
+          format = { enabled = false },
+          signatureHelp = { enabled = true },
+          contentProvider = { preferred = "fernflower" },
           codeGeneration = {
-            toString = {
-              template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-            },
-            hashCodeEquals = {
-              useJava7Objects = true,
-              useInstanceOf = true,
-            },
+            toString = { template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}" },
+            hashCodeEquals = { useJava7Objects = true, useInstanceOf = true },
             generateComments = true,
             useBlocks = true,
             addFinalForNewDeclaration = "fields",
@@ -83,54 +74,37 @@ return {
           implementationsCodeLens = { enabled = true },
           referencesCodeLens = { enabled = true },
           inlayHints = { parameterNames = { enabled = "all" } },
-          signatureHelp = { enabled = true },
-          sources = {
-            organizeImports = {
-              starThreshold = 9999,
-              staticStarThreshold = 9999,
-            },
-          },
+          sources = { organizeImports = { starThreshold = 9999, staticStarThreshold = 9999 } },
         },
       },
-      init_options = {
+      initOptions = {
+        workspace = workspace_dir,
         bundles = {
           vim.fn.expand "$MASON/share/java-debug-adapter/com.microsoft.java.debug.plugin.jar",
-          -- unpack remaining bundles
-          (table.unpack or unpack)(vim.split(vim.fn.glob "$MASON/share/java-test/*.jar", "\n", {})),
+          unpack(vim.split(vim.fn.glob "$MASON/share/java-test/*.jar", "\n", {})),
         },
+        extendedClientCapabilities = extendedClientCapabilities,
       },
       handlers = {
-        ["$/progress"] = function() end, -- disable progress updates.
+        ["language/status"] = function() end,
       },
       filetypes = { "java" },
       on_attach = function(...)
-        require("jdtls").setup_dap { hotcodereplace = "auto" }
-        local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
-        if astrolsp_avail then astrolsp.on_attach(...) end
-      end,
-    }, opts)
-  end,
-  config = function(_, opts)
-    -- setup autocmd on filetype detect java
-    vim.api.nvim_create_autocmd("Filetype", {
-      pattern = "java", -- autocmd to start jdtls
-      callback = function()
-        if opts.root_dir and opts.root_dir ~= "" then
-          require("jdtls").start_or_attach(opts)
-        else
-          require("astrocore").notify("jdtls: root_dir not found. Please specify a root marker", vim.log.levels.ERROR)
-        end
+        require("jdtls").setup_dap { hotcodereplace = "auto", config_overrides = {} }
+        require("astrolsp").on_attach(...)
       end,
     })
-    -- create autocmd to load main class configs on LspAttach.
-    -- This ensures that the LSP is fully attached.
-    -- See https://github.com/mfussenegger/nvim-jdtls#nvim-dap-configuration
+  end,
+  config = function(_, opts)
+    vim.api.nvim_create_autocmd("Filetype", {
+      pattern = "java",
+      callback = function() require("jdtls").start_or_attach(opts) end,
+    })
     vim.api.nvim_create_autocmd("LspAttach", {
-      pattern = "*.java",
+      pattern = "java",
       callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
-        -- ensure that only the jdtls client is activated
-        if client.name == "jdtls" then require("jdtls.dap").setup_dap_main_class_configs() end
+        if client and client.name == "jdtls" then require("jdtls.dap").setup_dap_main_class_configs() end
       end,
     })
   end,
