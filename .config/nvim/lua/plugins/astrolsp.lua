072ef575902ce788e900ffc04c6efdb2d28cf86d
Anton Ždanov <anton@azdanov.dev>
2025-09-30 10:45:24 +0300

    cleanup nvim

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -1,9 +1,3 @@
-local function to_uri(filepath)
-  filepath = vim.fs.normalize(filepath)
-  local absolute_path = vim.fn.fnamemodify(filepath, ":p")
-  return vim.uri_from_fname(absolute_path)
-end
-
 --- https://github.com/AstroNvim/astrolsp
 ---@type LazySpec
 return {
@@ -12,24 +6,6 @@ return {
   opts = {
     servers = { "sqruff" },
     autocmds = {
-      attach_css_custom_data = {
-        cond = function(client) return client.name == "cssls" end,
-        {
-          event = "LspAttach",
-          desc = "Attach custom data for CSS",
-          callback = function(args)
-            local success = vim.lsp.buf_notify(args.buf, "css/customDataChanged", {
-              { to_uri "~/.config/nvim/misc/tailwind.json" },
-            })
-            if success then
-              vim.defer_fn(function()
-                vim.diagnostic.reset(nil, args.buf)
-                vim.lsp.buf.document_highlight()
-              end, 0)
-            end
-          end,
-        },
-      },
       eslint_fix_on_save = {
         cond = function(client) return client.name == "eslint" and vim.fn.exists ":LspEslintFixAll" > 0 end,
         {
@@ -37,24 +13,6 @@ return {
           desc = "Fix all eslint errors",
           callback = function(args)
             if vim.F.if_nil(vim.b[args.buf].autoformat, vim.g.autoformat, true) then vim.cmd.LspEslintFixAll() end
-          end,
-        },
-      },
-      no_insert_inlay_hints = {
-        cond = vim.lsp.inlay_hint and "textDocument/inlayHint" or false,
-        {
-          event = "InsertEnter",
-          desc = "disable inlay hints on insert",
-          callback = function(args)
-            local filter = { bufnr = args.buf }
-            if vim.lsp.inlay_hint.is_enabled(filter) then
-              vim.lsp.inlay_hint.enable(false, filter)
-              vim.api.nvim_create_autocmd("InsertLeave", {
-                buffer = args.buf,
-                once = true,
-                callback = function() vim.lsp.inlay_hint.enable(true, filter) end,
-              })
-            end
           end,
         },
       },
--- .config/nvim/lua/plugins/leap.lua
+++
@@ -1,7 +0,0 @@
---- https://github.com/ggandor/leap.nvim
----@type LazySpec
-return {
-  "ggandor/leap.nvim",
-  dependencies = { "tpope/vim-repeat" },
-  config = function() require("leap").set_default_mappings() end,
-}
