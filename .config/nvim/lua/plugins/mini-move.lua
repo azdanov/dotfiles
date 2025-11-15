2f2a4dd754f36a8b527126a64973672512817f2c
Anton Ždanov <anton@azdanov.dev>
2025-11-15 19:24:29 +0200

    add mini move and snippets

---
+++ .config/nvim/lua/plugins/mini-move.lua
@@ -0,0 +1,12 @@
+--- https://github.com/nvim-mini/mini.move
+---@type LazySpec
+return {
+  "nvim-mini/mini.move",
+  keys = {
+    { "<M-l>", mode = { "n", "v" } },
+    { "<M-k>", mode = { "n", "v" } },
+    { "<M-j>", mode = { "n", "v" } },
+    { "<M-h>", mode = { "n", "v" } },
+  },
+  opts = {},
+}
