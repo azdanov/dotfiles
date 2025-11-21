d49211066a21c5f4b2f594a9c6db08c2c3468547
Anton Ždanov <anton@azdanov.dev>
2025-11-21 12:01:33 +0200

    remove vim-repeat

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -3,7 +3,6 @@
 return {
   "ggandor/leap.nvim",
   dependencies = {
-    "tpope/vim-repeat",
     {
       "AstroNvim/astrocore",
       opts = {
@@ -22,5 +21,23 @@ return {
       },
     },
   },
-  opts = {},
+  config = function()
+    -- Exclude whitespace and the middle of alphabetic words from preview:
+    --   foobar[baaz] = quux
+    --   ^----^^^--^^-^-^--^
+    require("leap").opts.preview = function(ch0, ch1, ch2)
+      return not (ch1:match "%s" or (ch0:match "%a" and ch1:match "%a" and ch2:match "%a"))
+    end
+
+    -- Define equivalence classes for brackets and quotes, in addition to the default whitespace group:
+    require("leap").opts.equivalence_classes = {
+      " \t\r\n",
+      "([{",
+      ")]}",
+      "'\"`",
+    }
+
+    -- Use the traversal keys to repeat the previous motion without explicitly invoking Leap:
+    require("leap.user").set_repeat_keys("<enter>", "<backspace>")
+  end,
 }
