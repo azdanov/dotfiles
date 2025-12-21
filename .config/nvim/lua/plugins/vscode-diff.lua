a6c8b6c6a0cea845a848db99f133a47f269607e0
Anton Ždanov <anton@azdanov.dev>
2025-12-21 15:53:14 +0200

    add vscode-diff

---
+++ .config/nvim/lua/plugins/vscode-diff.lua
@@ -0,0 +1,8 @@
+--- https://github.com/esmuellert/vscode-diff.nvim
+---@type LazySpec
+return {
+  "esmuellert/vscode-diff.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  cmd = "CodeDiff",
+  opts = {},
+}
