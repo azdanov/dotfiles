87e071ba4e6bcd769862bbf8ad804f363d00f651
Anton Ždanov <anton@azdanov.dev>
2026-01-26 18:53:39 +0200

    refactor fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,9 @@
 if status is-interactive
     mise activate fish | source
+    theme
 else
     mise activate fish --shims | source
 end
-
-abbr -a -- vi nvim
-abbr -a -- vim nvim
-abbr -a -- gg lazygit
-
-alias yl='lazygit --work-tree ~ --git-dir ~/.local/share/yadm/repo.git'
-
-set -gx EDITOR nvim
-set -gx MANPAGER 'nvim +Man!'
-
-set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
-
-# fish_config theme choose kanagawa-paper-ink
-
-set -e -g LS_COLORS FZF_DEFAULT_OPTS LG_CONFIG_FILE BAT_THEME
-dbus-update-activation-environment --systemd LS_COLORS FZF_DEFAULT_OPTS LG_CONFIG_FILE BAT_THEME
 
 starship init fish | source
 zoxide init fish | source
---
+++ .config/fish/functions/yl.fish
@@ -0,0 +1,3 @@
+function yl --wraps='lazygit --work-tree ~ --git-dir ~/.local/share/yadm/repo.git' --description 'alias yl=lazygit --work-tree ~ --git-dir ~/.local/share/yadm/repo.git'
+    lazygit --work-tree ~ --git-dir ~/.local/share/yadm/repo.git $argv
+end
