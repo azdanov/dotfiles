9a66fb0543b3c8b82c0c71a1314605f9a6ce2a17
Anton Ždanov <anton@azdanov.dev>
2026-01-26 17:38:10 +0200

    remove fzf opts

--- .config/fish/functions/theme_dark.fish
+++ .config/fish/functions/theme_dark.fish
@@ -3,7 +3,7 @@ function theme_dark
     sed -i s/kanagawa-paper-canvas/kanagawa-paper-ink/g ~/.config/nvim/lua/plugins/astroui.lua
 
     set -Ux LS_COLORS (vivid generate kanagawa-paper-ink)
-    set -Ux FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS \
+    set -Ux FZF_DEFAULT_OPTS " \
 --color=bg:-1,bg+:#2A2A37,fg:-1,fg+:#DCD7BA,hl:#938AA9,hl+:#c4746e \
 --color=header:#b6927b,info:#658594,pointer:#7AA89F \
 --color=marker:#7AA89F,prompt:#c4746e,spinner:#8ea49e
--- .config/fish/functions/theme_light.fish
+++ .config/fish/functions/theme_light.fish
@@ -3,7 +3,7 @@ function theme_light
     sed -i s/kanagawa-paper-ink/kanagawa-paper-canvas/g ~/.config/nvim/lua/plugins/astroui.lua
 
     set -Ux LS_COLORS (vivid generate kanagawa-paper-canvas)
-    set -Ux FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS \
+    set -Ux FZF_DEFAULT_OPTS " \
 --color=bg:-1,bg+:#d8d8d2,fg:-1,fg+:#73787d,hl:#9e7e98,hl+:#c27672 \
 --color=header:#b28d77,info:#809ba7,pointer:#516e7d \
 --color=marker:#516e7d,prompt:#c27672,spinner:#7e8faf
