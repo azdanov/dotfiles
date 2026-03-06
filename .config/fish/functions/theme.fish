f344a5623939e4c8a0ceba544b4c5aeefe9a8a16
Anton Ždanov <anton@azdanov.dev>
2026-03-06 20:43:30 +0200

    remove plasma

--- .config/fish/functions/theme.fish
+++ .config/fish/functions/theme.fish
@@ -20,8 +20,7 @@ function theme --on-variable fish_terminal_color_theme
             set fzf_opts " \
 --color=bg:-1,bg+:#d8d8d2,fg:-1,fg+:#73787d,hl:#9e7e98,hl+:#c27672 \
 --color=header:#b28d77,info:#809ba7,pointer:#516e7d \
---color=marker:#516e7d,prompt:#c27672,spinner:#7e8faf
-"
+--color=marker:#516e7d,prompt:#c27672,spinner:#7e8faf"
         case dark
             set theme kanagawa-paper-ink
             set btop_old "adwaita.theme"
@@ -33,8 +32,7 @@ function theme --on-variable fish_terminal_color_theme
             set fzf_opts " \
 --color=bg:-1,bg+:#2A2A37,fg:-1,fg+:#DCD7BA,hl:#938AA9,hl+:#c4746e \
 --color=header:#b6927b,info:#658594,pointer:#7AA89F \
---color=marker:#7AA89F,prompt:#c4746e,spinner:#8ea49e
-"
+--color=marker:#7AA89F,prompt:#c4746e,spinner:#8ea49e"
         case '*'
             return
     end
@@ -47,8 +45,4 @@ function theme --on-variable fish_terminal_color_theme
     rg -q "$btop_old" ~/.config/btop/btop.conf; and sed -i "s/$btop_old/$btop_new/g" ~/.config/btop/btop.conf
     rg -q "$nvim_old" ~/.config/nvim/lua/plugins/astroui.lua; and sed -i "s/$nvim_old/$nvim_new/g" ~/.config/nvim/lua/plugins/astroui.lua
 
-    set -l current_cursor (kreadconfig6 --file kcminputrc --group Mouse --key cursorTheme)
-    if test "$current_cursor" != "$cursor"
-        plasma-apply-cursortheme "$cursor" &>/dev/null
-    end
 end
