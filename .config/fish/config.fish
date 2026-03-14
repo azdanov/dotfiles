b5d4a33beaa0c1a1719822890eb4735e66adeb5b
Anton Ždanov <anton@azdanov.dev>
2026-03-14 20:37:05 +0200

    cleanup fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
+set -gx PAGER less
+set -gx EDITOR nvim
+set -gx MANPAGER 'nvim +Man!'
+set -gx fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+
 if status is-interactive
     $HOME/.local/bin/mise activate fish | source
     theme
@@ -5,5 +10,5 @@ else
     $HOME/.local/bin/mise activate fish --shims | source
 end
 
-starship init fish | source
 zoxide init fish | source
+starship init fish | source
