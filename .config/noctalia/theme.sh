715ea327bf4c4168a23eca8d578221a7470ec987
Anton Ždanov <anton@azdanov.dev>
2026-04-26 20:39:33 +0300

    update theme.sh

--- .config/noctalia/theme.sh
+++ .config/noctalia/theme.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
 niri msg action do-screen-transition
-#
-# darkMode=$(qs -c noctalia-shell ipc call state all | jq '.settings.colorSchemes.darkMode')
-#
-# if [ "$darkMode" = "true" ]; then
-#   gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-# else
-#   gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-# fi
+
+darkMode=$(qs -c noctalia-shell ipc call state all | jq -r '.settings.colorSchemes.darkMode')
+gtk_settings="$HOME/.config/gtk-4.0/settings.ini"
+
+if [ "$darkMode" = "true" ]; then
+  prefer_dark=1
+else
+  prefer_dark=0
+fi
+
+mkdir -p "$(dirname "$gtk_settings")"
+if [ ! -f "$gtk_settings" ]; then
+  printf "[Settings]\n" >"$gtk_settings"
+fi
+
+if grep -q '^gtk-application-prefer-dark-theme=' "$gtk_settings"; then
+  sed -i "s/^gtk-application-prefer-dark-theme=.*/gtk-application-prefer-dark-theme=$prefer_dark/" "$gtk_settings"
+else
+  printf "gtk-application-prefer-dark-theme=%s\n" "$prefer_dark" >>"$gtk_settings"
+fi
